af1964dce0f0fad4b5a2f39e71f354ee7b713889
choski91 <mlaura.smichowski@gmail.com>
2025-06-23 19:55:20 +0200

    hasta el ejercicio 3

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 //RESUELVE TUS EJERCICIOS AQUI
+
+//ejercicio 1
+
+function getAllBreeds() {
+    return fetch("https://dog.ceo/api/breeds/list/all")
+        .then(res => res.json())
+        .then(data => Object.keys(data.message));
+}
+// console.log(getAllBreeds());
+getAllBreeds().then(breeds => console.log(breeds));
+
+//ejercicio 2
+
+function getRandomDog() {
+    return fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res1 => res1.json())
+        .then(data1 => data1.message);
+}
+getRandomDog().then(breed => { console.log(breed); })
+
+//ejercicio 3
+
+function getAllImagesByBreed() {
+    return fetch("https://dog.ceo/api/breed/komondor/images")
+        .then(res2 => res2.json())
+        .then(data2 => data2.message);
+}
+
+getAllImagesByBreed().then(breed => { console.log(breed); })
+ 
