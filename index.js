255119cc6ce8b3254b55cb5515d171e7c4f8987f
choski91 <mlaura.smichowski@gmail.com>
2025-06-24 16:03:21 +0200

    hasta el ejercicio 5

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ getAllBreeds().then(breeds => console.log(breeds));
 
 function getRandomDog() {
     return fetch("https://dog.ceo/api/breeds/image/random")
-        .then(res1 => res1.json())
+        .then(res1 => res1.json()) // la funcion flecha tiene un return
         .then(data1 => data1.message);
 }
-getRandomDog().then(breed => { console.log(breed); })
+getRandomDog().then(breed => console.log(breed));
 
 //ejercicio 3
 
@@ -27,5 +27,42 @@ function getAllImagesByBreed() {
         .then(data2 => data2.message);
 }
 
-getAllImagesByBreed().then(breed => { console.log(breed); })
+getAllImagesByBreed().then(breed =>console.log(breed));
  
+//ejercicio 4
+function getAllImagesByBreed2(breed) {
+
+    let link = "https://dog.ceo/api/breed/"
+    let imagen = "/images"
+   return fetch (link + breed + imagen)
+   .then(res => res.json())
+   .then( datos => datos.message);
+}   
+
+getAllImagesByBreed().then(datos=>console.log(datos));
+
+//ejercicio 5
+
+function getGitHubUserProfile(username) {
+
+    return fetch (`https://api.github.com/users/${username}`)
+    .then(res => res.json())
+    .then(perfil => perfil);
+}
+
+getGitHubUserProfile("choski91").then( perfil=>console.log(perfil));
+
+
+//ejercicio 6
+
+function  printGithubUserProfile(username) {
+  return fetch
+  .then()
+  .then()
+}
+printGithubUserProfile().then(hola=>console.log());
+
+
+//ejercicio 7
+//ejercicio 8
+//ejercicio 9
